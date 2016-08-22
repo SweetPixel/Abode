c7aa4433268c0d8b99271dcbf2e00b61be2df01e
Farrukh Askari <farrukh.askari01@gmail.com>
2016-08-22 16:54:05 +0500

    player mouse controller
    added mouse control feature to player

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -98,6 +98,30 @@ public class PlayerMovement : MonoBehaviour {
 			}*/
 			//this.transform.Rotate(0, rotatePosX * rotationSpeed, 0);
 
+			// Generate a plane that intersects the transform's position with an upwards normal.
+			Plane playerPlane = new Plane(Vector3.up, transform.position);
+			// Generate a ray from the cursor position
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			// Determine the point where the cursor ray intersects the plane.
+			// This will be the point that the object must look towards to be looking at the mouse.
+			// Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
+			//   then find the point along that ray that meets that distance.  This will be the point
+			//   to look at.
+			float hitdist = 0.0f;
+			// If the ray is parallel to the plane, Raycast will return false.
+			if (playerPlane.Raycast (ray, out hitdist)) 
+			{
+				// Get the point along the ray that hits the calculated distance.
+				Vector3 targetPoint = ray.GetPoint(hitdist);
+
+				// Determine the target rotation.  This is the rotation if the transform looks at the target point.
+				Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+				// Smoothly rotate towards the target point.
+				transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, walkSpeed * Time.deltaTime);
+			}
+
 			if (!isStart) {
 				controlTimer -= Time.deltaTime;
 				if (controlTimer < 0) {
@@ -109,8 +133,8 @@ public class PlayerMovement : MonoBehaviour {
 				if (isStart) {
 					isStart = false;
 				}
-				Quaternion rotation = Quaternion.LookRotation (new Vector3 (rotatePosX, 0, rotatePostY));
-				transform.rotation = rotation;
+//				Quaternion rotation = Quaternion.LookRotation (new Vector3 (rotatePosX, 0, rotatePostY));
+//				transform.rotation = rotation;
 				transform.position += transform.forward * Time.deltaTime * walkSpeed;
 				gameObject.GetComponent<Animator> ().SetBool ("isMoving", true);
 
