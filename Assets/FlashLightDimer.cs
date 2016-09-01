7edd9b41d96f5f6f4fc80c4e6eedc107381ed11d
imrankhanswati <imrankhanswati315@gmail.com>
2016-09-01 18:01:17 +0500

    Light flow through wall is fixed

--- Assets/FlashLightDimer.cs
+++ Assets/FlashLightDimer.cs
@@ -5,6 +5,8 @@ public class FlashLightDimer : MonoBehaviour {
 
     public Transform startPos;
     public Transform targetPos;
+    public Transform targetLeft;
+    public Transform targetRight;
     public Light playerFlashLight;
     public float lightStrenght;
     private RaycastHit hit;
@@ -25,11 +27,11 @@ public class FlashLightDimer : MonoBehaviour {
             if (hit.collider.tag == "Walls")
             {
                 tempRange=lightStrenght * hit.distance;
-                if (tempRange < 13)
+                if (tempRange < 15)
                 {
-                    if (tempRange - 3 > 1)
+                    if (tempRange > 2.5)
                     {
-                        playerFlashLight.range = tempRange - 4;
+                            playerFlashLight.range = tempRange - 2;
                     }
                     else
                     {
@@ -38,18 +40,51 @@ public class FlashLightDimer : MonoBehaviour {
                     tmpResult = 10 - hit.distance;
                     if (playerFlashLight.intensity < tmpResult / 3)
                     {
-                        playerFlashLight.intensity = tmpResult/2;
+                        if (tmpResult / 1.7 > 2)
+                        {
+                            playerFlashLight.intensity = tmpResult / 1.7f;
+                            Debug.Log("done");
+                        }
+                        else
+                        {
+                            playerFlashLight.intensity = 2;
+                        }
                     }
                 }
+            }
+            else
+            {
+
+                playerFlashLight.range = Mathf.Lerp(playerFlashLight.range, 15, 0.5f);
+                playerFlashLight.intensity = Mathf.Lerp(playerFlashLight.intensity, 2f, 0.3f);
             }
         }
         else
         {
 
             playerFlashLight.range = Mathf.Lerp(playerFlashLight.range, 13, 0.5f);
-            playerFlashLight.intensity = Mathf.Lerp(playerFlashLight.intensity, 1.2f, 0.5f);
-            
+            playerFlashLight.intensity = Mathf.Lerp(playerFlashLight.intensity, 1.2f, 0.3f);
         }
-        //Debug.DrawLine(startPos.position, targetPos.position, Color.cyan);
+        if(Physics.Linecast(startPos.position,targetLeft.position,out hit))
+        {
+            if (hit.collider.tag == "Walls")
+            {
+                playerFlashLight.spotAngle = 65;
+            }
+        }
+        else if (Physics.Linecast(startPos.position, targetLeft.position, out hit))
+        {
+            if (hit.collider.tag == "Walls")
+            {
+                playerFlashLight.spotAngle = 65;
+            }
+        }
+        else
+        {
+            playerFlashLight.spotAngle = Mathf.Lerp(70, 80, 0.2f);
+        }
+        Debug.DrawLine(startPos.position, targetPos.position, Color.cyan);
+        Debug.DrawLine(startPos.position, targetLeft.position, Color.cyan);
+        Debug.DrawLine(startPos.position, targetRight.position, Color.cyan);
 	}
 }
